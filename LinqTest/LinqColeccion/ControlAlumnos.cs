55f89af5c2746065d762fbf0518393624fb6632d
Alvaro Barradas <116465602+AlvaroBarradas@users.noreply.github.com>
2023-05-18 11:25:56 -0600

    Add files via upload

---
+++ LinqTest/LinqColeccion/ControlAlumnos.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqColeccion
+{
+    internal class ControlAlumnos
+    {
+        List<Carrera> carreras = new List<Carrera>();
+        List<Alumno> alumnos = new List<Alumno>();
+
+        public ControlAlumnos()
+        {
+            carreras.Add(new Carrera { IdCarrera = 1, Nombre = "Ingenieria de Software" });
+            carreras.Add(new Carrera { IdCarrera = 2, Nombre = "Redes y servicio de computo"});
+
+            alumnos.Add(new Alumno { IdAlumno = 1, Nombre = "Juan Perez Sanchez", Matricula = "s090032", Promedio = 8.9, IdCarrera = 1});
+            alumnos.Add(new Alumno { IdAlumno = 2, Nombre = "Maria Rodriguez Martinez", Matricula = "s4324234", Promedio = 9.2, IdCarrera = 2 });
+            alumnos.Add(new Alumno { IdAlumno = 3, Nombre = "Jose Trelles Jimenez", Matricula = "s2111111", Promedio = 6.5, IdCarrera = 2 });
+        }
+        public void obtenerAlumnosPorCarrera(int idCarrera)
+        {
+            IEnumerable<Alumno> resultado = from alumno in alumnos join carrera in carreras on alumno.IdCarrera equals carrera.IdCarrera where alumno.IdCarrera == idCarrera select new { alumno, carrera };
+            foreach(Alumno alumno in resultado)
+            {
+                alumno.getDatosAlumno();
+                carrera.getDatosCarrera();
+            }
+        }
+    }
+}
---
+++ LinqTest/LinqColeccion/Program.cs
@@ -0,0 +1,24 @@
+﻿using LinqColeccion;
+using System.Linq;
+//Seleccion numeros pares
+int[] numeros = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+/*Console.WriteLine("A continuacionte mostramos los pares que existen en el arreglo");
+//Sin LINQ
+/*List<int> numerosPares = new List<int>();
+foreach (var numero in numeros)
+{
+    if(numero % 2 == 0)
+    {
+        numerosPares.Add(numero);
+    }
+}*/
+//Con LINQ
+/*IEnumerable<int> numerosPares = from numero in numeros where numero % 2 == 0 select numero;
+
+//Impresion numeros pares
+foreach (var numero in numerosPares)
+{
+    Console.WriteLine(numero);
+}*/
+ControlAlumnos controlAlumnos = new ControlAlumnos();
+controlAlumnos.obtenerAlumnosPorCarrera(2);
